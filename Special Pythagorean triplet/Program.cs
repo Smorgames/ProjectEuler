7d04a037b7c48d999287ea2a9c58efc7b3c02094
Ivan Zaveryukha <Zaverivan@gmail.com>
2020-10-30 21:10:32 +0300

    Add solutions
    1) Special Pythagorean triplet
    2) Summation of primes (shitty algorithm, which need to improve)

---
+++ Special Pythagorean triplet/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Special_Pythagorean_triplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c = 0;
+
+            int topLimit = 1000;
+
+            for (int i = 1; i <= topLimit; i++)
+            {
+                for (int j = 1; j <= topLimit; j++)
+                {
+                    a = i;
+                    b = j;
+                    c = topLimit - a - b;
+
+                    if (IsItPythagoreanTriplet(a, b, c) && a > b)
+                    {
+                        Console.WriteLine($"a = {a}, b = {b}, c = {c} and a * b * c = {a * b * c}");
+                        Console.WriteLine($"a^2 + b^2 = {a * a + b * b}");
+                        Console.WriteLine($"c^2 = {c * c}");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static bool IsItPythagoreanTriplet(int a, int b, int c)
+        {
+            if (a * a + b * b == c * c)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Summation of primes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Summation_of_primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int topLimit = 2000000;
+
+            int primeSum = 0;
+
+            for (int i = 2; i < topLimit; i++)
+            {
+                if (IsNumberPrime(i))
+                    primeSum += i;
+            }
+
+            Console.WriteLine(primeSum);
+        }
+
+        private static bool IsNumberPrime(int number)
+        {
+            int factorAmount = 1;
+            int divider = number - 1;
+
+            if (!FastPrimeCheck(number, 2) || !FastPrimeCheck(number, 3) || !FastPrimeCheck(number, 5) || !FastPrimeCheck(number, 7) || !FastPrimeCheck(number, 11))
+                return false;
+
+            while (factorAmount == 1 && divider != 1)
+            {
+                if (number / divider == number / (float)divider)
+                    factorAmount++;
+
+                divider--;
+            }
+
+            if (factorAmount == 1)
+                return true;
+            else
+                return false;
+        }
+
+        private static bool FastPrimeCheck(int number, int divider)
+        {
+            if (number % divider == 0 && number != divider)
+                return false;
+            else
+                return true;
+        }
+    }
+}
