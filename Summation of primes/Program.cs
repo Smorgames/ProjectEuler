1e78f8474fda69e369a3c9b901d8c000b48aedfb
Smorgames <Zaverivan1@gmail.com>
2020-11-01 13:19:28 +0300

    Add 2 solutions for "Summation of primes"
    1) Shitty algorythm works 5 minutes for 2.000.000
    2) Sieve of Eratosthenes works 2 sec

--- Summation of primes/Program.cs
+++ Summation of primes/Program.cs
@@ -6,33 +6,30 @@ namespace Summation_of_primes
     {
         static void Main(string[] args)
         {
-            int topLimit = 2000000;
+            int topLimit = 3000000;
 
-            int primeSum = 0;
+            //ShittyAlgorithmForPrimeSum(topLimit);
 
-            for (int i = 2; i < topLimit; i++)
-            {
-                if (IsNumberPrime(i))
-                    primeSum += i;
-            }
-
-            Console.WriteLine(primeSum);
+            SieveOfEratosthenes(topLimit);
         }
 
         private static bool IsNumberPrime(int number)
         {
             int factorAmount = 1;
-            int divider = number - 1;
+            int divider = 2;
 
             if (!FastPrimeCheck(number, 2) || !FastPrimeCheck(number, 3) || !FastPrimeCheck(number, 5) || !FastPrimeCheck(number, 7) || !FastPrimeCheck(number, 11))
                 return false;
 
-            while (factorAmount == 1 && divider != 1)
+            if (number == 2 || number == 3)
+                return true;
+
+            while (factorAmount == 1 && divider != number / 2)
             {
                 if (number / divider == number / (float)divider)
                     factorAmount++;
 
-                divider--;
+                divider++;
             }
 
             if (factorAmount == 1)
@@ -47,6 +44,106 @@ namespace Summation_of_primes
                 return false;
             else
                 return true;
+        }     
+
+        private static void ShittyAlgorithmForPrimeSum(int topLimit)
+        {
+            ulong primeSum = 0;
+
+            int primeCounter = 0;
+
+            for (int i = 2; i < topLimit; i++)
+            {
+                if (IsNumberPrime(i))
+                {
+                    primeSum += (ulong)i;
+                    primeCounter++;
+                }
+            }
+
+            DisplayPrimeAmountAndPrimeSum(topLimit, primeCounter, primeSum);
+        }
+
+        private static void SieveOfEratosthenes(int topLimit)
+        {
+            int[] numbers = new int[topLimit - 1];
+            int prime = 2;
+            ulong primeSum = 0;
+            int primeCounter = 0;
+
+            for (int j = 0; j < numbers.Length; j++)
+                numbers[j] = j + 2;
+
+            while ((int)MathF.Pow(prime, 2) < topLimit)
+            {
+                int primeIndex = GetIndex(numbers, prime);
+
+                for (int i = primeIndex + prime * (prime - 1); i < numbers.Length; i++)
+                    if (numbers[i] % prime == 0)
+                        numbers[i] = 0;
+
+                int interprime = prime;
+                int counter = GetIndex(numbers, prime) + 1;
+
+                while (prime == interprime)
+                {
+                    if (numbers[counter] == 0)
+                    {
+                        counter++;
+                        continue;
+                    }
+
+                    prime = numbers[counter];
+                    break;
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i] != 0)
+                {
+                    primeCounter++;
+                    primeSum += (ulong)numbers[i];
+                }
+
+
+            DisplayPrimeAmountAndPrimeSum(topLimit, primeCounter, primeSum, ConsoleColor.Red);
+        }
+
+        private static void DisplayPrimeAmountAndPrimeSum(int topLimit, int primeCounter, ulong primeSum)
+        {
+            Console.WriteLine($"Amount of primes less than {topLimit} = {primeCounter}");
+
+            Console.WriteLine(primeSum);
+        }
+
+        private static void DisplayPrimeAmountAndPrimeSum(int topLimit, int primeCounter, ulong primeSum, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            DisplayPrimeAmountAndPrimeSum(topLimit, primeCounter, primeSum);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static int GetIndex(int[] numbers, int value)
+        {
+            int index = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == value)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        private static void ColorMessege(string messege, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(messege);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
