f5b09753ffd38b894f9d61fff853df7ab6fa932b
Ivan Zaveryukha <Zaverivan@gmail.com>
2020-11-02 19:58:02 +0300

    Add piece of solution
    №12 Highly divisible triangular number

---
+++ Highly divisible triangular number/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Highly_divisible_triangular_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int triangularNumber = 1;
+
+            while (AmountOfNumberFactors(GetTriangularNumber(triangularNumber)) < 60)
+            {
+                Console.WriteLine($"{GetTriangularNumber(triangularNumber)} has {AmountOfNumberFactors(GetTriangularNumber(triangularNumber))} factors");
+                triangularNumber++;
+            }
+
+            Console.WriteLine(GetTriangularNumber(triangularNumber));
+        }
+
+        private static int GetTriangularNumber(int triangularLimit)
+        {
+            int triangularNumber = 0;
+
+            for (int i = 1; i <= triangularLimit; i++)
+                triangularNumber += i;
+
+            return triangularNumber;
+        }
+
+        private static int AmountOfNumberFactors(int number)
+        {
+            List<int> factors = new List<int>();
+
+            int factor = 1;
+            int mirrorFactor = number;
+
+            while (factor <= mirrorFactor)
+            {
+                if (IsNumberMultiples(number, factor))
+                {
+                    mirrorFactor = number / factor;
+                    factors.Add(factor);
+                    factors.Add(mirrorFactor);
+                }
+
+                factor++;
+            }
+
+            return factors.Count - 2;
+        }
+
+        private static bool IsNumberMultiples(int number, int divider)
+        {
+            if (number / divider == number / (float)divider)
+                return true;
+            else
+                return false;
+        }
+    }
+}
